0f263abb7c0b2cddc73aa7ce9592b3c7dd696ffa
Vivek Khurdhara <QD2097@QDS.COM>
2023-08-09 15:42:52 +0530

    new changes

--- storage/framework/views/de59e04d80ca809e7425bd69374c0358ffbbaf75.php
+++ storage/framework/views/de59e04d80ca809e7425bd69374c0358ffbbaf75.php
@@ -11,7 +11,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     
-    <title>Idfc</title>
+    <title>QDegree - People Progress Productivity</title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
